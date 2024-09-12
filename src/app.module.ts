f919067929bc778b0dd5e757de1467fdd59a7311
jonathan <jdavid598@hotmail.com>
2024-09-12 18:16:28 -0500

    cambio de conexion a mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { VehicleModule } from './vehicles/infrastructure/vehicle.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports:[VehicleModule,  MongooseModule.forRoot('mongodb://127.0.0.1:27017/Api-Carts')]
+  imports:[VehicleModule,  MongooseModule.forRoot('mongodb://3.23.79.145/Api-Carts')]
   //127.0.0.1:27017 uri de conexion local mongodb://127.0.0.1:27017/Api-Carts || mongodb://mongo:27017/Api-Carts mongodb://host.docker.internal:27017/Api-Carts
 })
 export class AppModule {}
