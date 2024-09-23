5e264cc85fd76387cf02c9a66eaf315d96339aff
jonathan <jdavid598@hotmail.com>
2024-09-23 18:37:43 -0500

    redis

--- src/vehicles/infrastructure/redis/redis.adapter.ts
+++ src/vehicles/infrastructure/redis/redis.adapter.ts
@@ -7,7 +7,8 @@ export class ElasticCacheAdapter {
 
     constructor() {
         this.client = createClient({
-            url: "redis-cache-paucki.serverless.use2.cache.amazonaws.com:6379"
+            url: "redis://redis-cache-paucki.serverless.use2.cache.amazonaws.com:6379"
+
         });
 
         this.client.connect().catch(err => {
