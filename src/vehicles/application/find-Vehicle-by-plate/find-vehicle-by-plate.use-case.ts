384ef389a8299f8a091eabd9d656659ca5cd7ef2
jonathan <jdavid598@hotmail.com>
2024-09-23 16:37:33 -0500

    h

--- src/vehicles/application/find-Vehicle-by-plate/find-vehicle-by-plate.use-case.ts
+++ src/vehicles/application/find-Vehicle-by-plate/find-vehicle-by-plate.use-case.ts
@@ -1,22 +1,35 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { PrimitiveVehicle } from "src/vehicles/domain/vehicle";
 import { VehicleNotFoundException } from "src/vehicles/domain/vehicle-not-found.exception";
 import { VehicleRepository } from "src/vehicles/domain/vehicle.repository";
 import {findVehicleByPlateDto } from "src/vehicles/application/find-Vehicle-by-plate/find-vehicle-by-plate.dto"
+import { RedisPort } from "src/vehicles/infrastructure/redis/redis.port.dto";
 
 @Injectable()
 export class findVehicleByPlateUseCase{
     constructor( 
         private readonly vehicleRepository:VehicleRepository,
+        @Inject("CachePort") private readonly cache: RedisPort
     ) {}
 
     async execute( findVehicleByPlateDto: findVehicleByPlateDto): Promise <{ vehicle: PrimitiveVehicle }>{
+
+        const cacheKey = `vehicle:${findVehicleByPlateDto.plate}`
+        const cacheVehicle = await this.cache.get(cacheKey)
+
+        if(cacheVehicle){
+            return {
+                vehicle: cacheVehicle
+            }
+        }
         
         const vehicle = await this.vehicleRepository.getByPlate(findVehicleByPlateDto.plate)
 
         if(!vehicle){
             throw new VehicleNotFoundException(findVehicleByPlateDto.plate)
         }
+
+        await this.cache.set(cacheKey, vehicle.toValue())
         
         return {
             vehicle: vehicle.toValue()
--- src/vehicles/infrastructure/vehicle.module.ts
+++ src/vehicles/infrastructure/vehicle.module.ts
@@ -8,17 +8,22 @@ import { VehicleRepository } from "../domain/vehicle.repository";
 import { findVehicleByPlateController } from "./find-vehicle-by-plate/find-vehicle-by-plate.controller";
 import { findVehicleByPlateUseCase } from "../application/find-Vehicle-by-plate/find-vehicle-by-plate.use-case";
 import { SQSService } from "src/sqs.services";
+import { ElasticCacheAdapter } from "./redis/redis.adapter";
 
 @Module({
     imports:[MongooseModule.forFeature([{ name: Vehicle.name, schema: VehicleSchema }])],
     controllers:[CreateVehicleController, findVehicleByPlateController],
-    providers:[ CreateVehicleUseCase, MongoVehicleRepository, findVehicleByPlateUseCase, CreateVehicleUseCase, SQSService,
+    providers:[ CreateVehicleUseCase, MongoVehicleRepository, findVehicleByPlateUseCase, CreateVehicleUseCase, SQSService, 
         {
             provide: VehicleRepository,
             useExisting: MongoVehicleRepository
-        },
+        }, ElasticCacheAdapter , 
+        {
+            provide:"CachePort",
+            useExisting: ElasticCacheAdapter
+        }
     ],
-    exports: [ CreateVehicleUseCase, findVehicleByPlateUseCase ]
+    exports: [ CreateVehicleUseCase, findVehicleByPlateUseCase, "CachePort"  ]
 
 })
 export class VehicleModule {} 
