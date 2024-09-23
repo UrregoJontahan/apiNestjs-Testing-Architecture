0093be38466d1fde744b5cf7930160eb2c63aef2
jonathan <jdavid598@hotmail.com>
2024-09-23 11:57:43 -0500

    s{

--- src/vehicles/infrastructure/redis/redis.adapter.ts
+++ src/vehicles/infrastructure/redis/redis.adapter.ts
@@ -7,6 +7,7 @@ export class RedisAdapter implements RedisPort {
   private client: RedisClientType;
 
   constructor() {
+  
     this.client = createClient({
       socket: {
         host: '3.142.221.120',
@@ -18,10 +19,12 @@ export class RedisAdapter implements RedisPort {
       console.error('Redis error', err);
     });
 
+
     this.client.connect().catch((err) => {
       console.error('Redis connection error:', err);
     });
   }
+
 
   async get(key: string): Promise<any> {
     try {
@@ -32,6 +35,7 @@ export class RedisAdapter implements RedisPort {
       throw err;
     }
   }
+
 
   async set(key: string, value: any): Promise<void> {
     try {
