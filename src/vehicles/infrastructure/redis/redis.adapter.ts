8b7fdaf8a89f064dacce2aa48dd3b5c395adaaf0
jonathan <jdavid598@hotmail.com>
2024-09-23 17:11:48 -0500

    listi

--- src/vehicles/infrastructure/redis/redis.adapter.ts
+++ src/vehicles/infrastructure/redis/redis.adapter.ts
@@ -1,40 +1,40 @@
 import { Injectable } from "@nestjs/common";
-import * as redis from "redis";
+import { createClient, RedisClientType } from 'redis';
 
 @Injectable()
 export class ElasticCacheAdapter {
-    private client;
+    private client: RedisClientType;
 
-    constructor(){
-        this.client = redis.createClient({
-            url: "redis-cache-paucki.serverless.use2.cache.amazonaws.com:6379"
+    constructor() {
+        this.client = createClient({
+            url: "redis://redis-cache-paucki.serverless.use2.cache.amazonaws.com:6379"
+        });
+
+        this.client.connect().catch(err => {
+            console.error("Error connecting to ElastiCache:", err);
         });
 
         this.client.on("error", (err) => {
-            console.error("elastic error", err)
-        })
+            console.error("ElastiCache error:", err);
+        });
     }
 
-    async get(key:string): Promise<any> {
-        return new Promise((resolve , reject) => {
-            this.client.get(key, (err, data) => {
-                if(err){
-                    return reject(err)
-                }
-
-                resolve(data ? JSON.parse(data) : null)
-            })
-        })
+    async get(key: string): Promise<any> {
+        try {
+            const data = await this.client.get(key);
+            return data ? JSON.parse(data) : null;
+        } catch (err) {
+            console.error("Error getting value from ElastiCache:", err);
+            throw err;
+        }
     }
 
-    async set(key:string, value:any): Promise<void> {
-        return new Promise ((resolve, reject) => {
-            this.client.set(key, JSON.stringify(value), (err) => {
-                if(err) {
-                    reject(err)
-                }
-                resolve()
-            })
-        })
+    async set(key: string, value: any): Promise<void> {
+        try {
+            await this.client.set(key, JSON.stringify(value));
+        } catch (err) {
+            console.error("Error setting value in ElastiCache:", err);
+            throw err;
+        }
     }
 }
