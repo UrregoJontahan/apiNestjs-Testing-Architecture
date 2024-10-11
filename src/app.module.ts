e363a85f3fac6c968a743e0e6787b5baf45cd45b
jonathan <jdavid598@hotmail.com>
2024-10-10 20:49:08 -0500

    o

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VehicleModule } from './vehicles/infrastructure/vehicle.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { SQSService } from './sqs.services';
 
 @Module({
-  imports:[VehicleModule,  MongooseModule.forRoot('mongodb://3.17.153.163:27017/Api-Carts')],
+  imports:[VehicleModule],
   providers:[SQSService],
   exports:[SQSService],
 })
